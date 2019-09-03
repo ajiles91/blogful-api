c3c1b21b789ec3dab362da5b88aab05ef7b71e7d
Amber <ajiles91@gmail.com>
2019-09-03 17:25:49 -0400

    added migration and tests

--- src/app.js
+++ src/app.js
@@ -4,15 +4,38 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+     .then(articles => {
+         res.json(articles)
+        })
+    .catch(next)    
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+      .then(article => {
+        if (!article) {
+          return res.status(404).json({
+            error: { message: `Article doesn't exist` }
+          })
+        }
+        res.json(article)
+      })
+      .catch(next)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
@@ -28,8 +51,5 @@ app.use(function errorHandler(error, req, res, next) {
     }
     res.status(500).json(response)
 })
-
-app.use(cors())
-
 
 module.exports = app
